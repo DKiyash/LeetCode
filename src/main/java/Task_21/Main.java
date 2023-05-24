29b40dc9e242320bbef173f4dd19ed4f1ad7cb7e
DKiyash <dkiyashenko@gmail.com>
2023-05-24 09:15:51 +0200

    Added Task_04

--- src/main/java/Task_21/Main.java
+++ src/main/java/Task_21/Main.java
@@ -2,15 +2,18 @@ package Task_21;
 
 public class Main {
     public static void main(String[] args) {
-        int[] list1 = {1, 2, 4};
-        int[] list2 = {1, 3, 4};
+//        int[] list1 = {1, 2, 4};
+//        int[] list2 = {1, 3, 4};
 //        int[] list1 = {};
 //        int[] list2 = {};
 //        int[] list1 = {};
 //        int[] list2 = {0};
 
-        ListNode listNode1 = arrayToNode(list1);
-        ListNode listNode2 = arrayToNode(list2);
+        ListNode listNode1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+        ListNode listNode2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+
+//        ListNode listNode1 = arrayToNode(list1);
+//        ListNode listNode2 = arrayToNode(list2);
         printNode(listNode1);
         System.out.println();
         printNode(listNode2);
